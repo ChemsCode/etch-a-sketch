f6f9816f4863e78c3d02d184f8235959f0234ebb
ChemsCode <saidiceb@gmail.com>
2022-01-22 14:22:33 -0500

    added dynamically sized grid-elements

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
+function squareDimensions(nOfSquares){
+    return Math.sqrt((Math.pow(768,2))/(Math.pow(nOfSquares,2)))
+}
+
+function makeGrid(nOfSquares){
+
+    let squareDim = squareDimensions(nOfSquares);
+
+    for(let i = 0 ; i < (nOfSquares*nOfSquares); i++){
+        let gridElement = document.createElement("div");
+        gridElement.style.width = `${squareDim}px`;
+        gridElement.style.height = `${squareDim}px`;
+        gridElement.classList.add("gridSquare");
+        container.appendChild(gridElement);
+    }
+
+}
 
 const container = document.querySelector("#container");
-const clearBtn = document.querySelector("#clearBtn")
+const clearBtn = document.querySelector("#clearBtn");
 
-for(let i = 0 ; i < (16*16); i++){
-    let gridElement = document.createElement("div");
-    gridElement.classList.add("gridSquare");
-    container.appendChild(gridElement);
-}
+let numberOfSquares = 64;
+const containerDimension = 768;
+makeGrid(numberOfSquares);
 
 const gridSquares = document.querySelectorAll(".gridSquare")
 
 
-for(let i = 0 ; i < 16*16; i++){
+for(let i = 0 ; i < numberOfSquares*numberOfSquares; i++){
     gridSquares[i].addEventListener("mouseover", () => {
         gridSquares[i].style.backgroundColor = "black"; 
     });
 }
 
 clearBtn.addEventListener("click", ()=>{
-    for(let i = 0 ; i < 16*16; i++){
+    for(let i = 0 ; i < numberOfSquares*numberOfSquares; i++){
         gridSquares[i].style.backgroundColor = "white"; 
     }
 });
